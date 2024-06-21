90f32e61501f554884f78c3eb4cd99f59b177476
soydz <dev@soydz.com>
2024-06-21 16:28:44 -0500

    Solución problema con CORS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 const app = express()
 
@@ -7,6 +8,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res[header] :body'))
 app.use(express.json())
+app.use(cors())
 
 let data = require('./data.json')
 const noData = {
