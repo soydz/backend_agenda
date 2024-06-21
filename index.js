b982cfc9fb6bd0d5193a980e57f61973babe8fb2
soydz <dev@soydz.com>
2024-06-21 17:52:55 -0500

    Se modifica el puerto del servidor para ser configurado por variable de entorno

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 
-const PORT = 3025
+const PORT = process.env.PORT || 3025
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
