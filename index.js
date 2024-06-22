31a198bb13d12b062759c31e451d46820b4b568b
soydz <dev@soydz.com>
2024-06-21 21:12:55 -0500

    Se modifica el servidor para servir código estático

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :res[header] :body'))
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 let data = require('./data.json')
 const noData = {
